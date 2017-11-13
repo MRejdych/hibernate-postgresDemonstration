8ac1109bf16da5a858facf67af4a6c579fe740a5
marcinrejdych <rejdych.marcin.1@gmail.com>
2017-11-13 20:27:50 +0100

    mr: added spring IOC container config

--- src/main/java/app/ApplicationConfiguration.java
+++ src/main/java/app/ApplicationConfiguration.java
@@ -3,16 +3,15 @@ package app;
 import app.queries.CustomersManipulator;
 import app.queries.EntitiesManipulator;
 import app.queries.ProductsManipulator;
+import app.utils.CustomerBuilder;
 import app.utils.DatabaseUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import app.utils.ProductBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan("app")
-public class ApplicationContext {
+public class ApplicationConfiguration {
 
     @Bean
     @Scope("prototype")
@@ -22,16 +21,26 @@ public class ApplicationContext {
 
     @Bean
     @Scope("prototype")
-    @Autowired
-    public EntitiesManipulator customersManipulator(){
-        return new CustomersManipulator();
+    public CustomersManipulator customersManipulator(){
+        return new CustomersManipulator(databaseUtils());
     }
 
     @Bean
     @Scope("prototype")
-    @Autowired
-    public EntitiesManipulator productsManipulator(){
-        return new ProductsManipulator();
+    public ProductsManipulator productsManipulator(){
+        return new ProductsManipulator(databaseUtils());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public CustomerBuilder customerBuilder(){
+        return new CustomerBuilder();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ProductBuilder productBuilder(){
+        return new ProductBuilder();
     }
 
 }
