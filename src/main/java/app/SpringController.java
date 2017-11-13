3c4f0307f4a7f3d09b83eeebce529542cb3c761f
marcinrejdych <rejdych.marcin.1@gmail.com>
2017-11-13 20:29:41 +0100

    mr: added mapping to SpringController

--- src/main/java/app/SpringController.java
+++ src/main/java/app/SpringController.java
@@ -2,6 +2,8 @@ package app;
 
 import app.entities.Customer;
 import app.queries.CustomersManipulator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Controller
 public class SpringController {
+    ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+
 
     @RequestMapping("/customers")
     public String showCustomersPage() {
@@ -18,7 +22,7 @@ public class SpringController {
 
     @RequestMapping("/customers/selectAll")
     public String showCustomersSelectAllPage(Model model) {
-        CustomersManipulator cm = new CustomersManipulator();
+        CustomersManipulator cm = context.getBean(CustomersManipulator.class);
         List<Customer> customers = cm.selectAllCustomers();
         model.addAttribute("customers", customers);
         model.addAttribute("nativeQueryTime", cm.getNativeSqlQueryTime());
