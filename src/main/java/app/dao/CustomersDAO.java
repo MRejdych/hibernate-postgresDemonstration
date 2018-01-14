2fd3832c1aaf40ef6133a29fe016ce3c850df657
marcinrejdych <rejdych.marcin.1@gmail.com>
2018-01-14 23:42:40 +0100

    category and supplier at products view

--- src/main/java/app/dao/CustomersDAO.java
+++ src/main/java/app/dao/CustomersDAO.java
@@ -42,10 +42,12 @@ public class CustomersDAO extends EntitiesDAO<Customer> {
 
     @Override
     public Customer readById(short customerId){
-        TypedQuery<Customer> generatedQuery = em.createQuery("SELECT c FROM Customer c WHERE c.customerId = ?1", Customer.class);
-        generatedQuery.setParameter(1, customerId);
+        return executeQueryAndSaveStatistics(() -> {
+            TypedQuery<Customer> generatedQuery = em.createQuery("SELECT c FROM Customer c WHERE c.customerId = ?1", Customer.class);
+            generatedQuery.setParameter(1, customerId);
 
-        return executeQueryAndSaveStatistics(generatedQuery::getSingleResult, READ);
+            return generatedQuery.getSingleResult();
+        }, READ);
     }
 
     @Override
@@ -61,81 +63,100 @@ public class CustomersDAO extends EntitiesDAO<Customer> {
 
     @Override
     public void delete(short customerId){
-        TypedQuery<Customer> generatedQuery = em.createQuery("DELETE FROM Customer c WHERE c.customerId = ?1", Customer.class);
-        generatedQuery.setParameter(1, customerId);
-        executeQueryAndSaveStatistics(generatedQuery::executeUpdate, DELETE);
+        executeQueryAndSaveStatistics(() -> {
+            TypedQuery<Customer> generatedQuery = em.createQuery("DELETE FROM Customer c WHERE c.customerId = ?1", Customer.class);
+            generatedQuery.setParameter(1, customerId);
+            generatedQuery.executeUpdate();
+        }, DELETE);
     }
 
     @Override
     public void createUsingNativeSql(Customer customer) {
-        String query =
-                "INSERT into customers(company_name, contact_name, contact_title, address, city, postal_code, country, region, phone, fax)" +
-                        "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10);";
+        executeQueryAndSaveStatistics(() -> {
 
-        Query nativeQuery = em.createNativeQuery(query, Customer.class);
-        nativeQuery.setParameter(1, customer.getCompanyName())
-                .setParameter(2, customer.getContactName())
-                .setParameter(3, customer.getContactTitle())
-                .setParameter(4, customer.getAddress().getAddress())
-                .setParameter(5, customer.getAddress().getCity())
-                .setParameter(6, customer.getAddress().getPostalCode())
-                .setParameter(7, customer.getAddress().getCountry())
-                .setParameter(8, customer.getRegion())
-                .setParameter(9, customer.getPhone())
-                .setParameter(10, customer.getFax());
+            String query =
+                    "INSERT into customers(company_name, contact_name, contact_title, address, city, postal_code, country, region, phone, fax)" +
+                    "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10);";
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_CREATE);
+            Query nativeQuery = em.createNativeQuery(query, Customer.class);
+            nativeQuery.setParameter(1, customer.getCompanyName())
+                    .setParameter(2, customer.getContactName())
+                    .setParameter(3, customer.getContactTitle())
+                    .setParameter(4, customer.getAddress().getAddress())
+                    .setParameter(5, customer.getAddress().getCity())
+                    .setParameter(6, customer.getAddress().getPostalCode())
+                    .setParameter(7, customer.getAddress().getCountry())
+                    .setParameter(8, customer.getRegion())
+                    .setParameter(9, customer.getPhone())
+                    .setParameter(10, customer.getFax());
+
+
+            nativeQuery.executeUpdate();
+
+        }, NATIVE_CREATE);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<Customer> readAllUsingNativeSql() {
-        Query nativeQuery = em.createNativeQuery("Select * From customers ORDER BY customer_id", Customer.class);
+        return executeQueryAndSaveStatistics(() -> {
 
-        return executeQueryAndSaveStatistics(() -> (List<Customer>) nativeQuery.getResultList(), NATIVE_READ);
+            Query nativeQuery = em.createNativeQuery("Select * From customers ORDER BY customer_id", Customer.class);
+
+            return (List<Customer>) nativeQuery.getResultList();
+        }, NATIVE_READ);
     }
 
     @Override
     public Customer readByIdUsingNativeSql(short customerId) {
-        Query nativeQuery = em.createNativeQuery("Select * From customers Where customer_id = ?1", Customer.class);
-        nativeQuery.setParameter(1, customerId);
+        return executeQueryAndSaveStatistics(() -> {
+            Query nativeQuery = em.createNativeQuery("Select * From customers Where customer_id = ?1", Customer.class);
+            nativeQuery.setParameter(1, customerId);
 
-        return executeQueryAndSaveStatistics(() -> (Customer) nativeQuery.getSingleResult(), NATIVE_READ);
+            return (Customer) nativeQuery.getSingleResult();
+        }, NATIVE_READ);
     }
 
 
 
+    @SuppressWarnings("Duplicates")
     @Override
     public void updateUsingNativeSql(Customer updatedCustomer, short customerId) {
-        String query =
-                "UPDATE customers SET company_name = (?1), contact_name = (?2), contact_title = (?3), address = (?4), " +
-                        "city = (?5), postal_code = (?6), country = (?7), region = (?8), phone = (?9), fax = (?10) " +
-                        "WHERE customer_id = (?11);";
+
+        executeQueryAndSaveStatistics(() -> {
+            String query =
+                    "UPDATE customers SET company_name = (?1), contact_name = (?2), contact_title = (?3), address = (?4), " +
+                            "city = (?5), postal_code = (?6), country = (?7), region = (?8), phone = (?9), fax = (?10) " +
+                            "WHERE customer_id = (?11);";
 
 
-        Query nativeQuery = em.createNativeQuery(query, Customer.class);
-        nativeQuery.setParameter(1, updatedCustomer.getCompanyName())
-                .setParameter(2, updatedCustomer.getContactName())
-                .setParameter(3, updatedCustomer.getContactTitle())
-                .setParameter(4, updatedCustomer.getAddress().getAddress())
-                .setParameter(5, updatedCustomer.getAddress().getCity())
-                .setParameter(6, updatedCustomer.getAddress().getPostalCode())
-                .setParameter(7, updatedCustomer.getAddress().getCountry())
-                .setParameter(8, updatedCustomer.getRegion())
-                .setParameter(9, updatedCustomer.getPhone())
-                .setParameter(10, updatedCustomer.getFax())
-                .setParameter(11, customerId);
+            Query nativeQuery = em.createNativeQuery(query, Customer.class);
+            nativeQuery.setParameter(1, updatedCustomer.getCompanyName())
+                    .setParameter(2, updatedCustomer.getContactName())
+                    .setParameter(3, updatedCustomer.getContactTitle())
+                    .setParameter(4, updatedCustomer.getAddress().getAddress())
+                    .setParameter(5, updatedCustomer.getAddress().getCity())
+                    .setParameter(6, updatedCustomer.getAddress().getPostalCode())
+                    .setParameter(7, updatedCustomer.getAddress().getCountry())
+                    .setParameter(8, updatedCustomer.getRegion())
+                    .setParameter(9, updatedCustomer.getPhone())
+                    .setParameter(10, updatedCustomer.getFax())
+                    .setParameter(11, customerId);
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_UPDATE);
+            nativeQuery.executeUpdate();
+        }, NATIVE_UPDATE);
 
     }
 
 
     @Override
     public void deleteUsingNativeSql(short customerId) {
-        Query nativeQuery = em.createNativeQuery("DELETE FROM customers WHERE customer_id = ?1", Customer.class);
-        nativeQuery.setParameter(1, customerId);
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_DELETE);
+        executeQueryAndSaveStatistics(() -> {
+            Query nativeQuery = em.createNativeQuery("DELETE FROM customers WHERE customer_id = ?1", Customer.class);
+            nativeQuery.setParameter(1, customerId);
+
+            nativeQuery.executeUpdate();
+        }, NATIVE_DELETE);
     }
 }
--- src/main/java/app/dao/ProductsDAO.java
+++ src/main/java/app/dao/ProductsDAO.java
@@ -21,20 +21,28 @@ public class ProductsDAO extends EntitiesDAO<Product> {
 
     @Override
     public void create(Product product) {
-        executeQueryAndSaveStatistics(() -> em.persist(product), CREATE);
+        executeQueryAndSaveStatistics(() -> {
+            em.persist(product);
+        }, CREATE);
     }
 
     public List<Product> readAll() {
-        TypedQuery<Product> generatedQuery = em.createQuery("SELECT pr FROM Product pr ORDER BY pr.productId", Product.class);
-        return executeQueryAndSaveStatistics(generatedQuery::getResultList, READ);
+        return executeQueryAndSaveStatistics(() -> {
+
+            TypedQuery<Product> generatedQuery = em.createQuery("SELECT pr FROM Product pr ORDER BY pr.productId", Product.class);
+
+            return generatedQuery.getResultList();
+        }, READ);
     }
 
     @Override
     public Product readById(short productId) {
-        TypedQuery<Product> generatedQuery = em.createQuery("SELECT pr FROM Product pr WHERE pr.productId = ?1", Product.class);
-        generatedQuery.setParameter(1, productId);
+        return executeQueryAndSaveStatistics(() -> {
+            TypedQuery<Product> generatedQuery = em.createQuery("SELECT pr FROM Product pr WHERE pr.productId = ?1", Product.class);
+            generatedQuery.setParameter(1, productId);
 
-        return executeQueryAndSaveStatistics(generatedQuery::getSingleResult, READ);
+            return generatedQuery.getSingleResult();
+        }, READ);
     }
 
     @Override
@@ -50,77 +58,96 @@ public class ProductsDAO extends EntitiesDAO<Product> {
 
     @Override
     public void delete(short productId) {
-        TypedQuery<Product> generatedQuery = em.createQuery("DELETE FROM Product pr WHERE pr.productId = ?1", Product.class);
-        generatedQuery.setParameter(1, productId);
+        executeQueryAndSaveStatistics(() -> {
 
-        executeQueryAndSaveStatistics((Runnable) generatedQuery::executeUpdate, DELETE);
+            TypedQuery<Product> generatedQuery = em.createQuery("DELETE FROM Product pr WHERE pr.productId = ?1", Product.class);
+            generatedQuery.setParameter(1, productId);
+
+            generatedQuery.executeUpdate();
+        }, DELETE);
     }
 
     @SuppressWarnings("StringBufferReplaceableByString")
     @Override
     public void createUsingNativeSql(Product product) {
-        String query =
-                "INSERT into products(product_name, quantity_per_unit, unit_price, units_in_stock, units_on_order," +
-                        " reorder_level, discontinued, supplier_id, category_id)" +
-                        "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9);";
+        executeQueryAndSaveStatistics(() -> {
 
-        Query nativeQuery = em.createNativeQuery(query, Product.class);
-        nativeQuery.setParameter(1, product.getProductName())
-                .setParameter(2, product.getQuantityPerUnit())
-                .setParameter(3, product.getUnitPrice())
-                .setParameter(4, product.getUnitsInStock())
-                .setParameter(5, product.getUnitsOnOrder())
-                .setParameter(6, product.getReorderLevel())
-                .setParameter(7, product.getDiscontinued())
-                .setParameter(8, product.getSupplier().getSupplierId())
-                .setParameter(9, product.getCategory().getCategoryId());
+            String query =
+                    "INSERT into products(product_name, quantity_per_unit, unit_price, units_in_stock, units_on_order," +
+                            " reorder_level, discontinued, supplier_id, category_id)" +
+                            "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9);";
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_CREATE);
+            Query nativeQuery = em.createNativeQuery(query, Product.class);
+            nativeQuery.setParameter(1, product.getProductName())
+                    .setParameter(2, product.getQuantityPerUnit())
+                    .setParameter(3, product.getUnitPrice())
+                    .setParameter(4, product.getUnitsInStock())
+                    .setParameter(5, product.getUnitsOnOrder())
+                    .setParameter(6, product.getReorderLevel())
+                    .setParameter(7, product.getDiscontinued())
+                    .setParameter(8, product.getSupplier().getSupplierId())
+                    .setParameter(9, product.getCategory().getCategoryId());
+
+            nativeQuery.executeUpdate();
+        }, NATIVE_CREATE);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<Product> readAllUsingNativeSql() {
-        Query nativeQuery = em.createNativeQuery("SELECT * FROM products ORDER BY product_id", Product.class);
+        return executeQueryAndSaveStatistics(() -> {
 
-        return executeQueryAndSaveStatistics(() -> (List<Product>) nativeQuery.getResultList(), NATIVE_READ);
+            Query nativeQuery = em.createNativeQuery("SELECT * FROM products ORDER BY product_id", Product.class);
+
+            return (List<Product>) nativeQuery.getResultList();
+        }, NATIVE_READ);
     }
 
     @Override
     public Product readByIdUsingNativeSql(short productId) {
-        Query nativeQuery = em.createNativeQuery("SELECT * FROM products WHERE product_id = ?1", Product.class);
-        nativeQuery.setParameter(1, productId);
+        return executeQueryAndSaveStatistics(() -> {
 
-        return executeQueryAndSaveStatistics(() -> (Product) nativeQuery.getSingleResult(), NATIVE_READ);
+            Query nativeQuery = em.createNativeQuery("SELECT * FROM products WHERE product_id = ?1", Product.class);
+            nativeQuery.setParameter(1, productId);
+
+            return (Product) nativeQuery.getSingleResult();
+        }, NATIVE_READ);
     }
 
+    @SuppressWarnings("Duplicates")
     @Override
     public void updateUsingNativeSql(Product updatedProduct, short productId) {
-        String query =
-                "UPDATE products SET product_name = (?1), quantity_per_unit = (?2), unit_price = (?3), units_in_stock = (?4)," +
-                        " units_on_order = (?5), reorder_level = (?6), discontinued = (?7), supplier_id = (?8), category_id = (?9)" +
-                        " WHERE product_id = (?10);";
 
-        Query nativeQuery = em.createNativeQuery(query, Product.class);
-        nativeQuery.setParameter(1, updatedProduct.getProductName())
-                .setParameter(2, updatedProduct.getQuantityPerUnit())
-                .setParameter(3, updatedProduct.getUnitPrice())
-                .setParameter(4, updatedProduct.getUnitsInStock())
-                .setParameter(5, updatedProduct.getUnitsOnOrder())
-                .setParameter(6, updatedProduct.getReorderLevel())
-                .setParameter(7, updatedProduct.getDiscontinued())
-                .setParameter(8, updatedProduct.getSupplier().getSupplierId())
-                .setParameter(9, updatedProduct.getCategory().getCategoryId())
-                .setParameter(10, productId);
+        executeQueryAndSaveStatistics(() -> {
+            String query =
+                    "UPDATE products SET product_name = (?1), quantity_per_unit = (?2), unit_price = (?3), units_in_stock = (?4)," +
+                            " units_on_order = (?5), reorder_level = (?6), discontinued = (?7), supplier_id = (?8), category_id = (?9)" +
+                            " WHERE product_id = (?10);";
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_UPDATE);
+            Query nativeQuery = em.createNativeQuery(query, Product.class);
+            nativeQuery.setParameter(1, updatedProduct.getProductName())
+                    .setParameter(2, updatedProduct.getQuantityPerUnit())
+                    .setParameter(3, updatedProduct.getUnitPrice())
+                    .setParameter(4, updatedProduct.getUnitsInStock())
+                    .setParameter(5, updatedProduct.getUnitsOnOrder())
+                    .setParameter(6, updatedProduct.getReorderLevel())
+                    .setParameter(7, updatedProduct.getDiscontinued())
+                    .setParameter(8, updatedProduct.getSupplier().getSupplierId())
+                    .setParameter(9, updatedProduct.getCategory().getCategoryId())
+                    .setParameter(10, productId);
+
+            nativeQuery.executeUpdate();
+        }, NATIVE_UPDATE);
     }
 
     @Override
     public void deleteUsingNativeSql(short productId) {
-        Query nativeQuery = em.createNativeQuery("DELETE FROM products WHERE product_id = ?1", Product.class);
-        nativeQuery.setParameter(1, productId);
 
-        executeQueryAndSaveStatistics(nativeQuery::executeUpdate, NATIVE_DELETE);
+        executeQueryAndSaveStatistics(() -> {
+            Query nativeQuery = em.createNativeQuery("DELETE FROM products WHERE product_id = ?1", Product.class);
+            nativeQuery.setParameter(1, productId);
+
+            nativeQuery.executeUpdate();
+        }, NATIVE_DELETE);
     }
 }
